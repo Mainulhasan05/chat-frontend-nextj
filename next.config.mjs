7f13a8f5860ecfc8c7c2ef54029f0c4483a9d1f9
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2024-07-25 18:00:41 +0600

    update api

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: "http://192.168.0.105:5000",
+    // API_URL: "http://192.168.0.105:5000",
+    API_URL: "https://chatapi.mainulhasan05.xyz",
   },
 };
 
